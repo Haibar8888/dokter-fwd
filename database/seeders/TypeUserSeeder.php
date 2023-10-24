bc12d05b82057f634796f0c85eaf46111c69c935
lalugaluh <lalugaluhhaibara@gmail.com>
2023-10-24 20:34:40 +0700

    folder resource done

--- database/seeders/TypeUserSeeder.php
+++ database/seeders/TypeUserSeeder.php
@@ -19,12 +19,12 @@ class TypeUserSeeder extends Seeder
         $type_user = [
             [
                 'name' => 'admin',
-                 'created_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'dokter',
-                 'created_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
