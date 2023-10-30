069c434f18fbcdf6a40533b45a22f3b8dc5973b4
Haibar <46429834+Haibar8888@users.noreply.github.com>
2023-10-30 10:12:48 +0800

    controller frontsite done

---
+++ app/Http/Controllers/Fronsite/LandingController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Fronsite;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Storage;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Auth\Access\Gate;
+
+use Illuminate\Support\Facades\Auth;
+
+// models
+use app\Models\User;
+use app\Models\MasterData\Specialist;
+use app\Models\Operational\Doctor;
+
+// third party package
+
+
+class LandingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        //
+        return view('pages.frontsite.landing-page.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store($request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Fronsite/PaymentController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Fronsite;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Storage;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Auth\Access\Gate;
+
+use Illuminate\Support\Facades\Auth;
+
+//use models here
+use app\Models\User;
+use app\Models\Operational\Doctor;
+use app\Models\MasterData\Consultation;
+use app\Models\MasterData\ConfigPayment;
+use app\Models\MasterData\Specialist;
+use app\Models\Operational\Transaction;
+use app\models\Operational\Appointment;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        //
+        return view('pages.frontsite.payment.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
